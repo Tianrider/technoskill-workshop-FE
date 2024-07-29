5b00ffbf17b34e9fbc8cea066486be9013366e9a
Tianrider <hadiwijayachristian7@gmail.com>
2024-07-29 17:19:42 +0700

    chore: Add lucide-react dependency and update font styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
 import HomePage from "./components/HomePage";
 import AddEmployeePage from "./components/AddEmployeePage";
@@ -6,23 +6,23 @@ import MyInfoPage from "./components/MyInfoPage";
 import LoginPage from "./components/LoginPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to={"/login"} />} />
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Navigate to={"/login"} />} />
 
-        <Route path="/home" element={<HomePage />} />
+				<Route path="/home" element={<HomePage />} />
 
-        <Route path="/add-employee" element={<AddEmployeePage />} />
+				<Route path="/add-employee" element={<AddEmployeePage />} />
 
-        <Route path="/my-info" element={<MyInfoPage />} />
+				<Route path="/my-info" element={<MyInfoPage />} />
 
-        <Route path="/login" element={<LoginPage />} />
+				<Route path="/login" element={<LoginPage />} />
 
-        {/* Input rute lain di sini */}
-      </Routes>
-    </BrowserRouter>
-  );
+				{/* Input rute lain di sini */}
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/components/elements/DashboardElement.jsx
+++ src/components/elements/DashboardElement.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 import homeIcon from "../../assets/home.svg";
 import addEmployeeIcon from "../../assets/addPerson.svg";
@@ -6,33 +6,40 @@ import myInfoIcon from "../../assets/person.svg";
 import loginIcon from "../../assets/login.svg";
 
 export default function DashboardElement() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  return (
-    <div className="bg-[#2B2E63] h-screen w-[390px] flex flex-col py-8">
-      <div className="flex ml-5" onClick={() => navigate("/my-info")}>
-        <img src={myInfoIcon} />
+	return (
+		<div className="bg-primary-gray h-screen w-[390px] flex flex-col py-8">
+			<div className="flex ml-5" onClick={() => navigate("/my-info")}>
+				<img src={myInfoIcon} />
 
-        <p className="my-auto text-white ml-5 text-[20px]">My Info</p>
-      </div>
+				<p className="my-auto text-white ml-5 text-[20px]">My Info</p>
+			</div>
 
-      <div className="flex ml-5 mt-5" onClick={() => navigate("/home")}>
-        <img src={homeIcon} />
+			<div className="flex ml-5 mt-5" onClick={() => navigate("/home")}>
+				<img src={homeIcon} />
 
-        <p className="my-auto text-white ml-5 text-[20px]">Home</p>
-      </div>
+				<p className="my-auto text-white ml-5 text-[20px] font-sf">
+					Home
+				</p>
+			</div>
 
-      <div className="flex ml-5 mt-5" onClick={() => navigate("/add-employee")}>
-        <img src={addEmployeeIcon} />
+			<div
+				className="flex ml-5 mt-5"
+				onClick={() => navigate("/add-employee")}
+			>
+				<img src={addEmployeeIcon} />
 
-        <p className="my-auto text-white ml-5 text-[20px]">Add Employee</p>
-      </div>
+				<p className="my-auto text-white ml-5 text-[20px]">
+					Add Employee
+				</p>
+			</div>
 
-      <div className="flex ml-5 mt-5" onClick={() => navigate("/login")}>
-        <img src={loginIcon} />
+			<div className="flex ml-5 mt-5" onClick={() => navigate("/login")}>
+				<img src={loginIcon} />
 
-        <p className="my-auto text-white ml-5 text-[20px]">Login</p>
-      </div>
-    </div>
-  );
+				<p className="my-auto text-white ml-5 text-[20px]">Login</p>
+			</div>
+		</div>
+	);
 }
